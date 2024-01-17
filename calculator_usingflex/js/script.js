afa10b71b7d29bf108095318f6d5456317d0ee2e
Robin Maharjan <robinmaharjan25@gmail.com>
2024-01-17 19:23:01 +0545

    added ac function

--- calculator_usingflex/js/script.js
+++ calculator_usingflex/js/script.js
@@ -1,25 +1,29 @@
 function calculator(event) {
-    let clickInput = event.target.innerHTML;
-    let output;
-    let screen = document.getElementById("screen");
-    if (
-        clickInput == "+" ||
-        clickInput == "-" ||
-        clickInput == "x" ||
-        clickInput == "/"
-    ) {
-        //operator
-    } else if (clickInput == "a") {
-        screen.innerHTML = "0";
-        console.log("clicked");
-    } else if (clickInput == "=") {
-        //equals
-    } else {
-        output = localStorage.getItem("previousInput");
-        if (output) {
-            output += clickInput;
-        }
-        localStorage.setItem("previousInput", output == null ? clickInput : output);
+  let clickInput = event.target.innerHTML;
+  let output;
+  let screen = document.getElementById("screen");
+  if (
+    clickInput == "+" ||
+    clickInput == "-" ||
+    clickInput == "x" ||
+    clickInput == "/"
+  ) {
+    //operator
+  } else if (clickInput == "=") {
+    //equals
+  } else {
+    output = localStorage.getItem("previousInput");
+    if (output) {
+      output += clickInput;
     }
-    screen.innerHTML = output == null ? clickInput : output;
+    localStorage.setItem("previousInput", output == null ? clickInput : output);
+  }
+  screen.innerHTML = output == null ? clickInput : output;
+}
+
+function ac() {
+  let screen = document.getElementById("screen");
+  screen.innerHTML = "0";
+  localStorage.clear();
+  console.log("clicked");
 }
